5156e80787efcd7dc34fc67f8481a7eed453a11b
jellicoe <dionhewson@gmail.com>
2024-05-14 23:53:17 +0000

    added bin and wait

--- clean_log.sh
+++ clean_log.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 rm /root/mainnet-docker/store/log/debug.log
-/root/mainnet-docker/up
+rm /root/mainnet-docker/store/db/transaction.db
+rm /root/mainnet-docker/store/db/ledger.db
+rm /root/mainnet-docker/store/db/nudb/nudb.dat
+
+wait 
+cd /root/mainnet-docker/
+/usr/bin/bash up
 
 echo `date`
